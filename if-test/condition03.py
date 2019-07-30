ec018a9d319d33e2cf553aa69033c61b9a9a8c13
DangineerIV <dadams757@yahoo.com>
2019-07-30 13:54:40 -0400

    if statements

--- if-test/condition03.py
+++ if-test/condition03.py
@@ -3,3 +3,12 @@
 hostname = input("What value should we set for hostname?")
 if hostname.lower() == "mtg":
     print("The hostname was found to be mtg")
+elif hostname.lower() != "mtg":
+    print("You suck again!")
+
+
+
+
+
+
+print("end script")
